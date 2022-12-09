ea48c5b95b63f51dbe55ea15884f9a0ed532c3be
Littlezoski <shicheng.zhou@my.utsa.edu>
2022-12-08 19:07:13 -0600

    spring class 4 lab OAUTH thru github

---
+++ MCC Spring class 4 lab/MCC-Spring-class-4-lab/src/main/java/MCC/Spring/class4/lab/MccSpringClass4LabApplication.java
@@ -0,0 +1,13 @@
+package MCC.Spring.class4.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccSpringClass4LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccSpringClass4LabApplication.class, args);
+	}
+
+}
---
+++ MCC Spring class 4 lab/MCC-Spring-class-4-lab/src/test/java/MCC/Spring/class4/lab/MccSpringClass4LabApplicationTests.java
@@ -0,0 +1,13 @@
+package MCC.Spring.class4.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccSpringClass4LabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
